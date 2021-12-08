e681364bd757837ac8f40e2024e0662579b026ab
Daniel Barros <daniel_barros@live.com>
2021-12-08 13:10:10 -0300

    Valida para Hero ser unico por token

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,5 +1,6 @@
 class Hero < ApplicationRecord
     validates :name, :token, presence: true
+    validates :name, uniqueness: { scope: :token, case_sensitive: false }
 
     scope :search, ->(term) { where('LOWER(name) LIKE ?', "%#{term.downcase}%") if term.present? }
     scope :sorted_by_name, -> { order(:name) }
